0cfd9eecf1486a0a350720c12ffba18caa372217
jeremysantana <santanajay21@gmail.com>
2020-10-26 15:14:23 -0400

    commit tree f3b9726ee833731ac35d77668a0a391520a27ea8

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array)
+    counter = 0 
+    new_array = []
+    while counter < array.length
+      new_array << yield(array[counter])
+      counter += 1 
+      end
+    new_array
+  end 
+  
